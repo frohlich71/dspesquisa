c2c344100066175467439fea8a23124bc348dee6
Victor Frohlich <victorfrohlich520@gmail.com>
2020-09-20 21:33:35 -0300

    Ädicionando rotas no React native

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import Header from '../../components/Header';
+
+
+const CreateRecord = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: '#FFF'
+    }
+  });
+
+export default CreateRecord;
